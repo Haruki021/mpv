7df90bc70164e0cad4c3eeadcbedf310f89ee158
Haruki021 <131473366+Haruki021@users.noreply.github.com>
2025-03-17 23:31:47 +0800

    Update misc.lua

--- portable_config/scripts/misc.lua
+++ portable_config/scripts/misc.lua
@@ -1,47 +1,21 @@
-local msg = require 'mp.msg'
 local utils = require 'mp.utils'
-
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
 mp.add_key_binding("e", "open-in-explorer", function()
-    local path = mp.get_property("path", ""):match("[\\\\%?\\]*(.+)") --windows下长路径前缀
-    if path and utils.file_info(path)["is_file"] then
-        path = string.gsub(path, "/", "\\")
+    local path = mp.get_property("path")
+    if utils.file_info(path or "") then
         mp.command_native({
             name = "subprocess",
             playback_only = false,
             detach = true,
-            args = { 'explorer', '/select,',  path .. ' ' },
+            args = { 'explorer', '/select,', path},
         })
     else
         mp.osd_message("Invalid: " ..(path or "No valid path found."), 3)
     end
 end)
-
 ---------------------------------------------------------------------------------------------
-
---[[-----------------------------------------------------------------------------------------
-mp.add_forced_key_binding("KP2", "playlist-prev-playlist", function()
-    local current_path = mp.get_property("playlist-path")
-    local current_pos = mp.get_property_number("playlist-pos")
-    local count = 0
-    if not current_path then mp.command("playlist-prev") return end
-
-    while(true) do
-        if count==2 then break end
-        if current_pos==0 then current_pos=-1 break end
-        current_pos = current_pos-1
-        if mp.get_property("playlist/"..current_pos.."/playlist-path") ~= current_path then
-            current_path = mp.get_property("playlist/"..current_pos.."/playlist-path")
-            count = count+1
-        end
-    end
-    mp.commandv("set", "playlist-pos", current_pos+1)
-end)
-]]--
---------------------------------------------------------------------------------------------
--- This script pauses playback when minimizing the window, and resumes playback
--- if it's brought back again. If the player was already paused when minimizing,
--- then try not to mess with the pause state.
-
+---------------------------------------------------------------------------------------------
 mp.observe_property("window-minimized", "bool", function(name, value)
     if value and not mp.get_property_bool("current-tracks/video/image") then
         mp.set_property_bool("pause", true)
