5aa94aabb2ec5556c34ac843fc89cf5d08c413c7
Haruki021 <131473366+Haruki021@users.noreply.github.com>
2025-03-21 02:45:27 +0800

    Update subload.lua

--- portable_config/scripts/subload.lua
+++ portable_config/scripts/subload.lua
@@ -3,21 +3,18 @@ local utils = require 'mp.utils'
 
 local function sub_path()
     local file_path = mp.get_property("path", "")
+    local fdir, pdir, sdir = file_path:match("((.-)([^/\\]-[/\\]?))[^/\\]*$")
     local dir_path = "sub,subs,subtitle,subtitles,字幕"
 
     --返回当前文件夹下字幕文件夹路径
-    local dirname = utils.split_path(file_path)
-    local subdirs = utils.readdir(dirname or "", "dirs") or {}
-    for _, v in ipairs(subdirs) do
+    for _, v in ipairs(utils.readdir(fdir, "dirs") or {}) do
         if dir_path:match(v:lower()) then
             return v
         end
     end
 
     --返回父级文件夹下字幕文件夹路径
-    local pdir, sdir = dirname:match("(.+[\\/])(.+[\\/])")
-    subdirs = utils.readdir(pdir or "", "dirs") or {}
-    for _, v in ipairs(subdirs) do
+    for _, v in ipairs(utils.readdir(pdir, "dirs") or {}) do
         if dir_path:match(v:lower()) then
             return utils.join_path("../"..v, sdir)
         end
