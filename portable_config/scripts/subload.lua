88d752063768ed01055af193298d7795d3b7ca06
Haruki021 <131473366+Haruki021@users.noreply.github.com>
2026-03-01 00:21:27 +0800

    Update subload.lua

--- portable_config/scripts/subload.lua
+++ portable_config/scripts/subload.lua
@@ -1,10 +1,10 @@
-local msg = require 'mp.msg'
 local utils = require 'mp.utils'
 
 local function sub_path()
     local file_path = mp.get_property("path", "")
     local fdir, pdir, sdir = file_path:match("((.-)([^/\\]-[/\\]?))[^/\\]*$")
     local dir_path = "sub,subs,subtitle,subtitles,字幕"
+    if mp.get_property("sub-file-paths"):match(sdir) then return end
 
     --返回当前文件夹下字幕文件夹路径
     for _, v in ipairs(utils.readdir(fdir, "dirs") or {}) do
@@ -25,7 +25,6 @@ local function load_subs()
     local path = sub_path()
     if path then mp.set_property("sub-file-paths", path) end
     --mp.commandv("change-list", "sub-file-paths", "set", path)
-    mp.unregister_event(load_subs)
 end
 
 mp.register_event("start-file", load_subs)
